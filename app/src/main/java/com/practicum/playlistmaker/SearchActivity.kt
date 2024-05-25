ca1db0ee6cbd10a265e940a533fe0970a0066dca
Igor Starkov <Kazesteam@yandex.ru>
2024-05-25 15:01:39 +0300

    выполнены все задания по 9 спринту

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,14 +1,86 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchTextValue = SEARCH_TEXT_VALUE
+    private lateinit var editTextSearch: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchBackButton = findViewById<ImageView>(R.id.searchBackButton)
+        editTextSearch = findViewById<EditText>(R.id.editTextSearch)
+        val imageViewSearchClear = findViewById<ImageView>(R.id.imageViewSearchClear)
+
+        searchBackButton.setOnClickListener {
+            finish()
+        }
+
+        imageViewSearchClear.setOnClickListener {
+            editTextSearch.setText("")
+            hideKeyBoard()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                imageViewSearchClear.visibility = imageViewSearchClearVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                searchTextValue = s?.toString() ?: ""
+            }
+
+        }
+
+        editTextSearch.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT_KEY, searchTextValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchTextValue = savedInstanceState.getString(SEARCH_TEXT_KEY, SEARCH_TEXT_VALUE)
+        editTextSearch.setText(searchTextValue)
+    }
+
+    private fun imageViewSearchClearVisibility(text: CharSequence?): Int {
+        return if (text.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun hideKeyBoard() {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
+    }
+
+    companion object {
+        const val SEARCH_TEXT_VALUE = ""
+        const val SEARCH_TEXT_KEY = "SEARCH_TEXT_KEY"
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,9 +12,40 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageView>(R.id.backButton)
+        val imageViewShare = findViewById<ImageView>(R.id.imageViewShare)
+        val imageViewSupport = findViewById<ImageView>(R.id.imageViewSupport)
+        val imageViewTerms = findViewById<ImageView>(R.id.imageViewTerms)
 
         backButton.setOnClickListener {
                 onBackPressedDispatcher.onBackPressed()
+        }
+
+        imageViewShare.setOnClickListener {
+            val url = Uri.parse(getString(R.string.share_uri))
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, url)
+            }
+            val chooser = Intent.createChooser(intent, getString(R.string.messenger_choose))
+            startActivity(chooser)
+        }
+
+        imageViewSupport.setOnClickListener {
+            val message = getString(R.string.support_message)
+            val subject = getString(R.string.support_subject)
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_mail)))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(intent)
+        }
+
+        imageViewTerms.setOnClickListener {
+            val url = Uri.parse(getString(R.string.terms_uri))
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            val chooser = Intent.createChooser(intent, getString(R.string.browser_choose))
+            startActivity(chooser)
         }
     }
 }
