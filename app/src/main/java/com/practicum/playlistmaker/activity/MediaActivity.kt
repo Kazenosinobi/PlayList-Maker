1ee245415aaaa9979436fcc37daeb4f3eb55b97c
Игорь Старков <Kazesteam@yandex.ru>
2024-08-05 15:37:12 +0300

    добавленны дебаунсеры и прогрессбар

--- app/src/main/java/com/practicum/playlistmaker/activity/MediaActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/activity/MediaActivity.kt
@@ -1,9 +1,11 @@
 package com.practicum.playlistmaker.activity
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
@@ -16,11 +18,12 @@ import kotlinx.serialization.json.Json
 class MediaActivity : AppCompatActivity() {
     private var backButton: ImageView? = null
     private var imageViewAlbum: ImageView? = null
-    private var textViewTrackName: TextView? = null
-    private var textViewArtistName: TextView? = null
     private var imageViewCatalog: ImageView? = null
     private var imageViewPlay: ImageView? = null
     private var imageViewFavourite: ImageView? = null
+
+    private var textViewTrackName: TextView? = null
+    private var textViewArtistName: TextView? = null
     private var textViewPlayTime: TextView? = null
     private var textViewDurationData: TextView? = null
     private var textViewAlbum: TextView? = null
@@ -30,6 +33,9 @@ class MediaActivity : AppCompatActivity() {
     private var textViewCountryData: TextView? = null
 
     private var track: Track? = null
+    private var mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+    private var url: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +45,30 @@ class MediaActivity : AppCompatActivity() {
         val jsonString = intent.getStringExtra(TRACK_KEY)
         track = jsonString?.let { Json.decodeFromString<Track>(it) }
 
+        url = track?.previewUrl
+        Log.d("MediaActivity", "$url")
+
         backButton?.setOnClickListener {
             finish()
         }
 
         getImageAlbum()
         setText()
+        preparePlayer()
+
+        imageViewPlay?.setOnClickListener {
+            playbackControl()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 
     private fun getImageAlbum() {
@@ -52,12 +76,8 @@ class MediaActivity : AppCompatActivity() {
         val coverUrl = track?.getCoverArtwork()
         Log.d("MediaActivity", "Cover URL: $coverUrl")
         imageViewAlbum?.let {
-            Glide.with(this)
-                .load(track?.getCoverArtwork())
-                .placeholder(R.drawable.place_holder)
-                .fitCenter()
-                .transform(RoundedCorners(cornerRadius))
-                .into(it)
+            Glide.with(this).load(track?.getCoverArtwork()).placeholder(R.drawable.place_holder)
+                .fitCenter().transform(RoundedCorners(cornerRadius)).into(it)
         }
     }
 
@@ -79,6 +99,42 @@ class MediaActivity : AppCompatActivity() {
         }
     }
 
+    private fun preparePlayer() {
+            mediaPlayer.setDataSource(url)
+            mediaPlayer.prepareAsync()
+            mediaPlayer.setOnPreparedListener {
+                imageViewPlay?.isEnabled = true
+                playerState = STATE_PREPARED
+            }
+            mediaPlayer.setOnCompletionListener {
+                playerState = STATE_PREPARED
+            }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        imageViewPlay?.setImageResource(R.drawable.pause_button)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        imageViewPlay?.setImageResource(R.drawable.play_button)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+            when (playerState) {
+                STATE_PLAYING -> {
+                    pausePlayer()
+                }
+
+                STATE_PREPARED, STATE_PAUSED -> {
+                    startPlayer()
+                }
+        }
+    }
+
     private fun initViews() {
         backButton = findViewById(R.id.backButton)
         imageViewAlbum = findViewById(R.id.imageViewAlbum)
@@ -94,5 +150,12 @@ class MediaActivity : AppCompatActivity() {
         textViewYearData = findViewById(R.id.textViewYearData)
         textViewGenreData = findViewById(R.id.textViewGenreData)
         textViewCountryData = findViewById(R.id.textViewCountryData)
+    }
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
     }
 }
