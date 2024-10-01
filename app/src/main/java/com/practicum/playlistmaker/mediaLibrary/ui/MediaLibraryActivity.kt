c94ad9d4609145258a6678c1e1136199fb1305bd
Игорь Старков <Kazesteam@yandex.ru>
2024-10-01 14:51:59 +0300

    fragments added

--- app/src/main/java/com/practicum/playlistmaker/mediaLibrary/ui/MediaLibraryActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/mediaLibrary/ui/MediaLibraryActivity.kt
@@ -4,29 +4,42 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayoutMediator
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityMediaLibraryBinding
 
 class MediaLibraryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMediaLibraryBinding
 
+    private lateinit var tabMediator: TabLayoutMediator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMediaLibraryBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.viewPager.adapter = MediaLibraryViewPagerAdapter(
+            fragmentManager = supportFragmentManager,
+            lifecycle = lifecycle,
+        )
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.favourite_tracks)
+                else -> tab.text = getString(R.string.play_lists)
+            }
         }
+        tabMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 
     companion object {
+
         fun createMediaLibraryActivityIntent(context: Context): Intent {
             return Intent(context, MediaLibraryActivity::class.java)
         }
